c9fd7936a891552555f5fda7dc38028262487e47
Lucas Ponce <ponce.ballesteros@gmail.com>
2017-06-06 09:38:24 +0200

    Copy 03 script file into the VM

--- 02-config-vm.sh
+++ 02-config-vm.sh
@@ -1,11 +1,10 @@
 LOGGING_HOSTNAME="logging.dev.hawkular.es"
-LOGGING_VM_IP="192.168.122.16"
-HOSTS_ENTRY="${LOGGING_VM_IP} ${LOGGING_HOSTNAME} openshift.${LOGGING_HOSTNAME} kibana.${LOGGING_HOSTNAME} mux.${LOGGING_HOSTNAME} hawkular-metrics.${LOGGING_HOSTNAME}"
+LOGGING_VM_IP="192.168.122.183"
+HOSTS_ENTRY="${LOGGING_VM_IP} ${LOGGING_HOSTNAME} openshift.${LOGGING_HOSTNAME} kibana.${LOGGING_HOSTNAME} mux.${LOGGING_HOSTNAME}"
 
 sudo su -c "echo $HOSTS_ENTRY >> /etc/hosts"
 ssh-copy-id root@${LOGGING_HOSTNAME}
 
-ssh root@${LOGGING_HOSTNAME}
-
 scp 03-install-from-vm.sh root@${LOGGING_HOSTNAME}:/root
 
+ssh root@${LOGGING_HOSTNAME}
