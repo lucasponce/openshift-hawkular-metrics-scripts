996289c4b22c8da6a5228b037e2f9e29201dc374
Lucas Ponce <ponce.ballesteros@gmail.com>
2017-06-06 08:49:21 +0200

    Missing variable

--- 02-config-vm.sh
+++ 02-config-vm.sh
@@ -1,5 +1,5 @@
 LOGGING_HOSTNAME="logging.dev.hawkular.es"
-LOGGING_VM_IP="192.168.122.170"
+LOGGING_VM_IP="192.168.122.16"
 HOSTS_ENTRY="${LOGGING_VM_IP} ${LOGGING_HOSTNAME} openshift.${LOGGING_HOSTNAME} kibana.${LOGGING_HOSTNAME} mux.${LOGGING_HOSTNAME} hawkular-metrics.${LOGGING_HOSTNAME}"
 
 sudo su -c "echo $HOSTS_ENTRY >> /etc/hosts"
@@ -7,5 +7,5 @@ ssh-copy-id root@${LOGGING_HOSTNAME}
 
 ssh root@${LOGGING_HOSTNAME}
 
-scp 03-install-from-vm.sh root@logging.dev.hawkular.es:/root
+scp 03-install-from-vm.sh root@${LOGGING_HOSTNAME}:/root
 
