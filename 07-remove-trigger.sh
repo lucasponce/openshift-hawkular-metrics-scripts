420d70f0d030caaa485172d47d771b3fa53e8abe
Lucas Ponce <ponce.ballesteros@gmail.com>
2017-06-09 13:25:41 +0200

    Add script to delete trigger

---
+++ 07-remove-trigger.sh
@@ -0,0 +1,12 @@
+LOGGING_VM_IP="192.168.122.4"
+HAWKULAR_HOST="hawkular-metrics.app.${LOGGING_VM_IP}.nip.io"
+HAWKULAR_TOKEN="P1iz0oyudhxwdyO891pvK9BGgWyqigy4Nt3r-nemTjo"
+HAWKULAR_PORT=443
+HAWKULAR_TENANT="my_tenant"
+
+curl -k -v \
+       -H "Hawkular-Tenant: ${HAWKULAR_TENANT}" \
+       -H "Authorization: Bearer ${HAWKULAR_TOKEN}" \
+       -H "Content-Type: application/json" \
+       -X DELETE \
+       https://${HAWKULAR_HOST}/hawkular/alerts/triggers/Logs-from-Logging 
